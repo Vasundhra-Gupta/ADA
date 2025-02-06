5c7c69a949a7bde716f5bc4242f0f75a35618daa
vasundhra gupta <vasundhragupta962@gmail.com>
2025-02-06 16:53:31 +0530

    fractional nap sack sorted acc to weights

---
+++ Questions/0402fractionalNapSack.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <time.h>
+#include "../headers/array2D.h"
+#include <vector>
+#include <chrono>
+using namespace chrono;
+using namespace std;
+// arr[i][0] = weight, arr[i][1] = profit
+void generateArray(int **arr, int n, int m)
+{
+    srand(time(0));
+    for (int i = 0; i < n; i++)
+    {
+        int wt = rand() % 200 + 20;
+        int profit = rand() % 1000 + 100;
+        arr[i][0] = wt;
+        arr[i][1] = profit;
+    }
+}
+
+int partition(int **arr, int low, int high)
+{
+    int pivot = arr[low][0];
+    int i = low + 1;
+    int j = high;
+    while (i <= j)
+    {
+        while (i <= high && arr[i][0] <= pivot)
+        {
+            i++;
+        };
+        while (j > low && arr[j][0] > pivot)
+        {
+            j--;
+        };
+        if (i < j)
+        {
+            swap(arr[i][0], arr[j][0]);
+            swap(arr[i][1], arr[j][1]);
+        }
+    };
+    swap(arr[low][1], arr[j][1]);
+    swap(arr[low][0], arr[j][0]);
+    // printArray(arr, high+1, 2);
+
+    return j;
+}
+void quickSort(int **arr, int low, int high)
+{
+    if (low < high)
+    {
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
+    }
+}
+
+vector<double> fractionalNapSack(int **arr, int n, int m)
+{
+    int capacity = 500;
+    int wt = 0;
+    int profit = 0;
+    vector<double> finalList(n, 0);
+    int i = 0;
+    while (wt <= capacity)
+    {
+        wt = wt + arr[i][0];
+        if (wt <= capacity)//so that extra profit cant be added!
+        {
+            profit = profit + arr[i][1];
+            finalList[i] = 1;
+            i++;
+        }
+    }
+    int remainWt = wt - capacity;
+    wt = wt - remainWt;
+    double fraction = (double)(arr[i][0] - remainWt) / arr[i][0];
+    profit = profit + fraction * arr[i][1];
+    finalList[i] = fraction;
+
+    if (capacity - wt > 0)
+    {
+        cout << "More stock can be bought\n";
+    }
+    return finalList;
+}
+
+int main()
+{
+    int n, m;
+    cout << "Enter row and columns for matrix:\n";
+    cin >> n >> m;
+    int **arr = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[m];
+    }
+    generateArray(arr, n, m);
+    quickSort(arr, 0, n - 1);
+    cout << "Weight\tProfit" << endl;
+    printArray(arr, n, m);
+    auto start = high_resolution_clock::now();
+    vector<double> list = fractionalNapSack(arr, n, m);
+    auto end = high_resolution_clock::now();
+    int profit=0, count = 0;
+    for (int i = 0; i < list.size(); i++)
+    {
+        cout << "item no. " << i + 1 << "'s selling status is " << list[i] << endl;
+        count++;
+        profit += arr[i][1];
+
+    }
+
+    auto duration = duration_cast<milliseconds>(end - start);
+    cout << "The duration is " << duration.count() << " milliseconds" << endl;
+    cout << "Total Profit is " << profit << " for " << count << " items";
+    return 0;
+}
