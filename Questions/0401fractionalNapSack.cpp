60595a132f5429da8dd4bd9ef62bd2856f408a38
vasundhra gupta <vasundhragupta962@gmail.com>
2025-02-06 16:53:09 +0530

    fractional nap sack sorted acc to profits

--- Questions/0401fractionalNapSack.cpp
+++ Questions/0401fractionalNapSack.cpp
@@ -5,42 +5,47 @@
 #include <chrono>
 using namespace chrono;
 using namespace std;
-//arr[i][0] = weight, arr[i][1] = profit
-void generateArray(int**arr, int n , int m){
+// arr[i][0] = weight, arr[i][1] = profit
+void generateArray(int **arr, int n, int m)
+{
     srand(time(0));
-    for(int i=0; i<n; i++){
-        int wt = rand() %200 + 5;
-        int profit = rand()%1000 + 100;
+    for (int i = 0; i < n; i++)
+    {
+        int wt = rand() % 200 + 5;
+        int profit = rand() % 1000 + 100;
         arr[i][0] = wt;
         arr[i][1] = profit;
     }
 }
 
-int partition(int** arr, int low, int high)
+int partition(int **arr, int low, int high)
 {
     int pivot = arr[low][1];
-    int i = low+1;      
+    int i = low + 1;
     int j = high;
-    while (i <= j){
-        while (i <= high && arr[i][1] <= pivot ){
+    while (i <= j)
+    {
+        while (i <= high && arr[i][1] <= pivot)
+        {
             i++;
-        } ;
-        while (j > low && arr[j][1] > pivot){
+        };
+        while (j > low && arr[j][1] > pivot)
+        {
             j--;
-        } ;
+        };
         if (i < j)
         {
             swap(arr[i][0], arr[j][0]);
             swap(arr[i][1], arr[j][1]);
         }
-    } ;
+    };
     swap(arr[low][1], arr[j][1]);
     swap(arr[low][0], arr[j][0]);
     // printArray(arr, high+1, 2);
 
     return j;
 }
-void quickSort(int** arr, int low, int high)
+void quickSort(int **arr, int low, int high)
 {
     if (low < high)
     {
@@ -50,53 +55,65 @@ void quickSort(int** arr, int low, int high)
     }
 }
 
-vector<double> fractionalNapSack(int** arr, int n, int m){
+vector<double> fractionalNapSack(int **arr, int n, int m)
+{
     int capacity = 500;
-    int wt = 0; 
+    int wt = 0;
     int profit = 0;
     vector<double> finalList(n, 0);
-    for(int i=0; i<n; i++){
-        if(wt<=capacity){
-            wt = wt + arr[i][0];
+    int i = 0;
+    while (wt <= capacity)
+    {
+        wt = wt + arr[i][0];
+        if (wt <= capacity)
+        {
             profit = profit + arr[i][1];
             finalList[i] = 1;
-        }else{
-            int remainWt = wt - capacity;
-            wt = wt+remainWt;
-            double fraction = (double)remainWt/arr[i][0];
-            profit = profit + fraction*arr[i][1];
-            finalList[i] = fraction;
-            break;
+            i++;
         }
     }
-    cout<<"Total capicity"<<capacity<<endl;
-    cout<<"Wait till recent item:"<<wt<<endl;
+    int remainWt = wt - capacity;
+    wt = wt - remainWt;
+    double fraction = (double)(arr[i][0] - remainWt) / arr[i][0];
+    profit = profit + fraction * arr[i][1];
+    finalList[i] = fraction;
 
-    if(capacity - wt > 0){
-        cout<<"More stock can be bought\n";
+    if (capacity - wt > 0)
+    {
+        cout << "More stock can be bought\n";
     }
     return finalList;
 }
 
-int main(){
+int main()
+{
     int n, m;
-    cout<<"Enter row and columns for matrix:\n";
-    cin>>n>> m;
-    int** arr = new int*[n];
-    for(int i=0; i<n; i++){
+    cout << "Enter row and columns for matrix:\n";
+    cin >> n >> m;
+    int **arr = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
         arr[i] = new int[m];
     }
     generateArray(arr, n, m);
-    quickSort(arr, 0, n-1);
+    quickSort(arr, 0, n - 1);
+    cout << "Weight\tProfit" << endl;
     printArray(arr, n, m);
     auto start = high_resolution_clock::now();
     vector<double> list = fractionalNapSack(arr, n, m);
     auto end = high_resolution_clock::now();
 
+    int count = 0, profit = 0;
+    for (int i = 0; i < list.size(); i++)
+    {
+        cout << "item no. " << i + 1 << "'s selling status is " << list[i] << endl;
+        count++;
+        profit += arr[i][1];
+    }
+
     auto duration = duration_cast<milliseconds>(end - start);
     cout << "The duration is " << duration.count() << " milliseconds" << endl;
-    for(int i=0; i<list.size(); i++){
-        cout<<"item no. "<<i+1<<"'s selling status is "<<list[i]<<endl;
-    }
+    cout << "Total Profit is " << profit << " for " << count << " items";
+
     return 0;
 }
