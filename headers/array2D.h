133205b6d62a8e65463bf0a2ac7af60c32adbdd1
vasundhra gupta <vasundhragupta962@gmail.com>
2025-02-23 16:33:44 +0530

    changes

--- headers/array2D.h
+++ headers/array2D.h
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-void inputSquaredArray(int** arr, int n)
+void inputSquaredArray(int **arr, int n)
 {
     cout << "Enter the elements of array:" << endl;
     for (int i = 0; i < n; i++)
@@ -15,11 +15,35 @@ void inputSquaredArray(int** arr, int n)
     }
 }
 
-void printArray(int** arr, int n, int m)
+void inputSquaredArray(vector<vector<int>> arr)
+{
+    cout << "Enter the elements of array:" << endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr.size(); j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+}
+
+void printArray(int **arr, int n, int m)
 {
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+void printArray(vector<vector<int>> arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
         {
             cout << arr[i][j] << "\t";
         }
