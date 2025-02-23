04a9690fbc445ebd041b4e5c71c9a082edb0de81
vasundhra gupta <vasundhragupta962@gmail.com>
2025-02-23 16:33:17 +0530

    iterative quick sort

--- Questions/07IterativeQuickSort.cpp
+++ Questions/07IterativeQuickSort.cpp
@@ -27,6 +27,44 @@ int partition(int arr[], int low, int high)
     return j;
 }
 
+// void quickSort(int arr[], int low, int high)
+// {
+//     stack<int> st;
+//     while (true)
+//     {
+//         while (low < high)
+//         {
+//             int j = partition(arr, low, high);
+//             if (j - low <= high - j)
+//             {
+//                 low = j + 1;
+//                 st.push(low);
+//                 st.push(high);
+//             }
+//             else
+//             {
+//                 high = j - 1;
+//                 st.push(low);
+//                 st.push(high);
+//             }
+//         }
+//         if (st.empty())
+//         {
+//             return;
+//         }
+//         else
+//         {
+//             high = st.top();
+//             st.pop();
+//             cout<<"high"<<high<<endl;
+//             low = st.top();
+//             st.pop();
+//             cout<<"low"<<low<<endl;
+//         }
+//         // cout<<st.size()<<endl;
+//     }
+// }
+
 void quickSort(int arr[], int low, int high)
 {
     stack<int> st;
@@ -37,15 +75,15 @@ void quickSort(int arr[], int low, int high)
             int j = partition(arr, low, high);
             if (j - low <= high - j)
             {
-                low = j + 1;
-                st.push(low);
+                st.push(j+1);
                 st.push(high);
+                high = j-1;
             }
             else
             {
-                high = j - 1;
                 st.push(low);
-                st.push(high);
+                st.push(j-1);
+                low = j + 1;
             }
         }
         if (st.empty())
@@ -59,19 +97,20 @@ void quickSort(int arr[], int low, int high)
             low = st.top();
             st.pop();
         }
-        cout<<st.size()<<endl;
+        // cout<<st.size()<<endl;
     }
 }
 
 int main()
 {
-    cout << "Enter size of an array" << endl;
-    int n;
-    cin >> n;
-    int *arr = new int[n];
-    generateArray(arr, n);
-    printArray(arr, n);
-    quickSort(arr, 0, n - 1);
-    printArray(arr, n);
+    // cout << "Enter size of an array" << endl;
+    // int n;
+    // cin >> n;
+    // int *arr = new int[n];
+    // generateArray(arr, n);
+    int arr [] = {5, 3, 1, 0 , 7, 0};
+    printArray(arr, 6);
+    quickSort(arr, 0,6-1);
+    printArray(arr, 6);
     return 0;
 }
