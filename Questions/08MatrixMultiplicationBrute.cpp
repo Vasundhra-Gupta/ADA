246d168183adc2a38162d08da81780a3a2a8cef0
vasundhra gupta <vasundhragupta962@gmail.com>
2025-02-27 13:31:22 +0530

    matrix multipliaction

--- Questions/08MatrixMultiplicationBrute.cpp
+++ Questions/08MatrixMultiplicationBrute.cpp
@@ -24,38 +24,41 @@ void multiply(vector<vector<int>> matrix1, vector<vector<int>> matrix2, vector<v
 }
 
 int main(){
-    int  row1, col1, row2, col2;
-    cout<<"Enter row of matrix 1"<<endl;
-    cin>>row1;
-    cout<<"Enter col of matrix 1"<<endl;
-    cin>>col1;
-    cout<<"Enter row of matrix 2"<<endl;
-    cin>>row2;
-    cout<<"Enter col of matrix 2"<<endl;
-    cin>>col2;
-    vector<vector<int>> matrix1(row1, vector<int>(col1, 0)), matrix2(row2, vector<int>(col2, 0)), result;
+    // int  row1, col1, row2, col2;
+    // cout<<"Enter row of matrix 1"<<endl;
+    // cin>>row1;
+    // cout<<"Enter col of matrix 1"<<endl;
+    // cin>>col1;
+    // cout<<"Enter row of matrix 2"<<endl;
+    // cin>>row2;
+    // cout<<"Enter col of matrix 2"<<endl;
+    // cin>>col2;
+    // vector<vector<int>> matrix1(row1, vector<int>(col1, 0)), matrix2(row2, vector<int>(col2, 0)), result;
 
-    // matrix1 ={
-    //     {2, 3, 1},
-    //     {4, 9, 1},
-    // };
-    // matrix2 ={
-    //     {1, 2},
-    //     {2, 1},
-    //     {1, 1}
-    // };
+    vector<vector<int>> matrix1 ={
+        {2, 3, 1},
+        {4, 9, 1},
+    };
+    vector<vector<int>> matrix2 ={
+        {1, 2},
+        {2, 1},
+        {1, 1}
+    };
+
+    vector<vector<int>> result;
     
-    if (row1 <= 0 || col1 <= 0 || row2 <= 0 || col2 <= 0) {
-        cout << "Matrix dimensions must be positive integers.\n";
-        return 0;
-    }
+    // if (row1 <= 0 || col1 <= 0 || row2 <= 0 || col2 <= 0) {
+    //     cout << "Matrix dimensions must be positive integers.\n";
+    //     return 0;
+    // }
+    // if (col1 != row2) {
+    //     cout << "Matrix multiplication is not possible with these dimensions.\n";
+    //     return 0;
+    // }
+    // input2DArray(matrix1, row1, col1);
+    // input2DArray(matrix2, row2, col2);
 
-    if (col1 != row2) {
-        cout << "Matrix multiplication is not possible with these dimensions.\n";
-        return 0;
-    }
-    input2DArray(matrix1, row1, col1);
-    input2DArray(matrix2, row2, col2);
+
     multiply(matrix1, matrix2, result);
     printArray(result);
     
