8e1725677e018ff694cc7ce504cbdc28ceeded92
vasundhra gupta <vasundhragupta962@gmail.com>
2025-05-23 18:56:47 +0530

    just init

--- Questions/hornersRule.cpp
+++ Questions/hornersRule.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int horner(int* &a, int i, int n, int &x){
+int horner(int* a, int i, int n, int &x){
     if(i==n-1){
         return a[i];
     }else{
@@ -8,8 +8,7 @@ int horner(int* &a, int i, int n, int &x){
     }
 }
 int main(){
-    // int a[]= {1, 2, 2, 1};
-    int* a = new int[4]{1, 2, 2, 1};
+    int a[]= {1, 2, 2, 1};
     int x=2;
     int result = horner(a, 0, 4, x);
     cout<<"result: "<<result<<endl;
